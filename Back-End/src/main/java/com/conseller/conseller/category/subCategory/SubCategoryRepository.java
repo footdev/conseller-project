65c9493a0854f2f6addca8560598839acbbfbd18
ki_mensoo <ekclstkfka5@gmail.com>
2024-07-23 19:45:09 +0900

    refactor: 서브 카테고리 메서드 추가

--- Back-End/src/main/java/com/conseller/conseller/category/subCategory/SubCategoryRepository.java
+++ Back-End/src/main/java/com/conseller/conseller/category/subCategory/SubCategoryRepository.java
@@ -1,8 +1,10 @@
 package com.conseller.conseller.category.subCategory;import com.conseller.conseller.entity.SubCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SubCategoryRepository extends JpaRepository<SubCategory, Integer> {
     Optional<SubCategory> findBySubCategoryIdx(int subCategory);
+    Optional<List<SubCategory>> findByMainCategoryMainCategoryIdx(int mainCategoryIdx);
 }
